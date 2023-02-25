38a0bed44f331d4062a685c2c7038136f6cdbaea
Andrew Cline <acline@precisionplanting.com>
2023-02-25 13:28:16 -0600

    chore(cli-ui): _ unused var

--- cli-ui/main.ts
+++ cli-ui/main.ts
@@ -1,6 +1,6 @@
 import { Command } from "https://deno.land/x/cliffy@v0.25.7/mod.ts";
 
-const cmd = await new Command()
+const _cmd = await new Command()
   .name("lithium")
   .version("0.1.0")
   .description("Commandline ui for lithium todos")
