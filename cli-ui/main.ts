ce99d3c122a5d3ed99e1decdd04e07373020b404
Andrew Cline <acline@precisionplanting.com>
2023-04-08 10:36:42 -0500

    feat: mv directories around

--- cli-ui/main.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "https://deno.land/x/cliffy@v0.25.7/mod.ts";
-
-const _cmd = await new Command()
-  .name("lithium")
-  .version("0.1.0")
-  .description("Commandline ui for lithium todos")
-  .parse(Deno.args);
-
-export const add = (a: number, b: number) => a + b;
